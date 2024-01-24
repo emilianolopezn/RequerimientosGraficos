fb12301e313aa181ceab9f8c3ee260b26b01bd87
Emiliano <emiliano@mawetecnologias.com>
2024-01-24 07:19:57 -0700

    Actualizar

--- setup.bat
+++ setup.bat
@@ -1,1 +1,1 @@
-C:\Users\alumno.USN\AppData\Local\Programs\Python\Python311\Scripts\pip install pyopengl glfw
+C:\Users\alumno\AppData\Local\Programs\Python\Python312\Scripts\pip install pyopengl glfw
